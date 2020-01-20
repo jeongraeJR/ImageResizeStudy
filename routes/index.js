8df07d3b710a217b7730d7e74cb4fd186fc1247c
jeongraeKim <kimjr@gananetworks.com>
2020-01-20 11:22:36 +0900

    image resize

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var formidable = require('formidable');
 var sharp = require('sharp');
+var util = require('util');
 
 router.get('/', function (req, res, next) {
 	res.render('index.html', { title: 'Express' });
@@ -9,22 +10,19 @@ router.get('/', function (req, res, next) {
 
 router.post('/upload', async function (req, res, next) {
 	const [fields, files] = await parseForm(req);
-	sharp(files.attach)
+	sharp(files.attach.path)
 		.resize(200, 300, {
 			kernel: sharp.kernel.nearest,
 			fit: 'contain',
 			position: 'right top',
 			background: { r: 255, g: 255, b: 255, alpha: 0.5 }
 		})
-		.toFile('output.jpg', function (err) {
-			if (err) console.log(err);
-			response.sendFile('output.jpg');
-		})
-		.then(() => {
-			// output.png is a 200 pixels wide and 300 pixels high image
-			// containing a nearest-neighbour scaled version
-			// contained within the north-east corner of a semi-transparent white canvas
-		});
+		.flatten()
+		.toFile(`./public/images/${files.attach.name}`)
+		.then((data) => {
+			res.send(data);
+		}); 
+
 })
 
 
